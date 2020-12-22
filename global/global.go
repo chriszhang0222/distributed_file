a552ebb0a08a30ff884bb1d32262984547ebde32
chriszhang94 <chris@quantumsds.com>
2020-12-22 16:15:42 -0500

    read from config.yaml

--- global/global.go
+++ global/global.go
@@ -1,11 +1,27 @@
 package global
-import "distributed_file/config"
+import (
+	"distributed_file/config"
+	"fmt"
+	//"github.com/nacos-group/nacos-sdk-go/clients"
+	//"github.com/nacos-group/nacos-sdk-go/common/constant"
+	//"github.com/nacos-group/nacos-sdk-go/vo"
+	"github.com/spf13/viper"
+)
 
 var (
-	NacosConfig *config.NacosInit
-	ServerConfig *config.ServerConfig
+	NacosConfig *config.NacosInit = &config.NacosInit{}
+	ServerConfig *config.ServerConfig = &config.ServerConfig{}
 )
 
 func init(){
-
+	configFile := "./config.yaml"
+	v := viper.New()
+	v.SetConfigFile(configFile)
+	if err := v.ReadInConfig(); err != nil{
+		panic(err)
+	}
+	if err := v.Unmarshal(NacosConfig);err != nil{
+		panic(err)
+	}
+	fmt.Println(NacosConfig.Nacos)
 }
--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"distributed_file/db/orm"
-	"fmt"
+	_ "distributed_file/global"
 	_ "github.com/go-sql-driver/mysql"
 )
 func main(){
 
-	exc := orm.GetUserInfo("chris")
-	fmt.Println(exc.Data)
 
 
 }
