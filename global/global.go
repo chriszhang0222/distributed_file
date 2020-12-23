434a6c1d76b1b9cfb3a5dc1b7d6c37bcb0f8d65d
chriszhang94 <chris@quantumsds.com>
2020-12-22 19:13:55 -0500

    read config from nacos

--- global/global.go
+++ global/global.go
@@ -1,9 +1,12 @@
 package global
+
 import (
 	"distributed_file/config"
-	"fmt"
-	//"github.com/nacos-group/nacos-sdk-go/clients"
-	//"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"encoding/json"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+
 	//"github.com/nacos-group/nacos-sdk-go/vo"
 	"github.com/spf13/viper"
 )
@@ -23,5 +26,45 @@ func init(){
 	if err := v.Unmarshal(NacosConfig);err != nil{
 		panic(err)
 	}
-	fmt.Println(NacosConfig.Nacos)
+	readFromnacos()
+
+}
+
+func readFromnacos(){
+	nacosInfo := NacosConfig.Nacos
+	sc := []constant.ServerConfig{
+		{
+			IpAddr: nacosInfo.Host,
+			Port: uint64(nacosInfo.Port),
+		},
+	}
+	cc := constant.ClientConfig{
+		NamespaceId: nacosInfo.Namespace,
+		TimeoutMs: 5000,
+		NotLoadCacheAtStart: true,
+		LogDir: "tmp/nacos/log",
+		CacheDir: "tmp/nacos/cache",
+		RotateTime: "1h",
+		MaxAge: 3,
+		LogLevel: "debug",
+	}
+	configClient, err := clients.CreateConfigClient(map[string]interface{}{
+		"serverConfigs": sc,
+		"clientConfig":  cc,
+	})
+	if err != nil{
+		panic(err)
+	}
+	content, err := configClient.GetConfig(
+		vo.ConfigParam{
+			DataId: nacosInfo.DataId,
+			Group: nacosInfo.Group,
+		})
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal([]byte(content), &ServerConfig)
+	if err != nil {
+		panic(err)
+	}
 }
